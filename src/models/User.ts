32dcbbba5ee07fd5b6bb32f875beb75fc58003d9
Manoj Ranga <manojranga7583@gmail.com>
2023-12-01 15:59:12 +0530

    feat: implement user getters and setters functions

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,5 @@
 export class User {
-    private readonly name: string;
+    private name: string;
     private email: string;
 
     constructor(name: string, email: string) {
@@ -14,4 +14,14 @@ export class User {
     public manageLibrary(): void {
         console.log("Managing the library");
     }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
 }
+
